00291a47b597c7821f02beac0975b2b353729e28
Destiny Erhabor <destinterhabor6@gmail.com>
2021-06-27 01:08:03 +0100

    created the database entry

---
+++ database/db.js
@@ -0,0 +1,23 @@
+// chalk is a package that just add colors to output, nothing special. Don't get confuse with it.
+const chalk = require("chalk");
+const mongoose = require("mongoose");
+
+module.exports.db = async () => {
+  try {
+    const connectMongoose = await mongoose.connect(
+      process.env.MONGO_URL,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    if (!connectMongoose) {
+      console.log(chalk.red("Database failed to connect"));
+    }
+    console.log(chalk.green("Database Connected correctly"));
+  } catch (err) {
+    console.log("error occur");
+    console.log(err);
+  }
+};
